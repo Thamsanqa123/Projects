58d160c3ab9f3346b29b363f0e73901b779c322d
thamsanqa123 <thamsanqatshabangu2@gmail.com>
2026-03-16 15:23:52 +0200

    "the page is able to store client business card" "user is able to search clients business anytime"

--- Business card generator/app.js
+++ Business card generator/app.js
@@ -1,9 +1,15 @@
 
-let clients = []
+let clients = JSON.parse(localStorage.getItem('clients')) || []
 
 const clientInfo = document.querySelector("#form")
     const businessCar = document.querySelector('#businessCard')
 const generateBtn = document.querySelector('#generate')
+
+const saveClients = ()=>{
+localStorage.setItem('clients', JSON.stringify(clients))
+  
+}
+    
 
 
 clientInfo.addEventListener('submit', (e)=>{
@@ -19,7 +25,7 @@ let details = {
 
 clients.push(details)
 
-
+saveClients(details)
 displayCard(details)
 console.log(clients)
 e.target.reset()
@@ -37,11 +43,40 @@ Email: ${client.email}`
 }
 
 
-const searchCard = ()=>{
-    
+const input = document.querySelector('.searchInput')
+const clientCard = document.querySelector(".clientCard")
+
+input.addEventListener('input', (e)=>{
+    e.preventDefault()
+
+    const value = e.target.value
+    if(value === ''){
+clientCard.innerHTML='';
+return;
+    }
+
+    const results = clients.filter((client)=>{
+return client.name.toLowerCase().includes(value.toLowerCase()) || 
+(client.subject && client.subject.toLowerCase().includes(value.toLowerCase()))
+    })
+   
+     displaySearch(results)
+})
+
+const displaySearch = (results)=>{
+
+if(results.length === 0){
+    return clientCard.innerHTML= '<p>Client card not found</p>'
 }
 
-
+results.forEach(client => {
+    html = `<h3> ${client.subject} </h3>
+Name: ${client.name} <br>
+Business Name: ${client.subject} <br>
+Email: ${client.email}`
+});
+clientCard.innerHTML= html
+}
 
 
 
