d5ab6c3161ccbb2701b2f825554fcbc2fd3f2c65
thamsanqa123 <thamsanqatshabangu2@gmail.com>
2026-03-20 13:31:15 +0200

    New mini project
    added for loop to calculate grades

---
+++ Grade Calculator/app.js
@@ -0,0 +1,40 @@
+
+
+
+// const results = grades.filter((e)=>{
+
+//    if(e <= 50) {
+//     console.log(`lowest grade: ${e}`)
+//    }else if(e === 50 || e===53){
+// console.log(`Average: ${e}`)
+//    }
+//    else if(e >= 50){
+// console.log(`Highest: ${e}`)
+//    }
+// })
+let grades = [85, 75, 67, 84, 53, 40, 70, 30]
+let lowestGrades= []
+    let averageGrd= []
+
+    let highestGrd= []
+
+
+for(let i = 0; i< grades.length; ++i){
+   
+    if(grades[i] <= 50){
+    lowestGrades.push(grades[i])
+    return 'A'
+    }
+    else if(grades[i] === 50 || grades[i] <=53){
+ averageGrd.push(grades[i])
+   
+    } else if (grades[i] >=60){
+ highestGrd.push(grades[i])
+  
+    }
+}
+console.log(`lowest grades: ${lowestGrades}`)
+console.log(`average grades: ${averageGrd}`)
+console.log(`highest grades: ${highestGrd}`)
+
+
